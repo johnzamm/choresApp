9e75b6cae5f6a0dbb4abd4132251f9f3f73679e9
Jmay <justin.sean.may@gmail.com>
2018-06-22 20:25:09 -0400

    gradient + white text

--- Chores_iOS/Chores_iOS/ViewController.swift
+++ Chores_iOS/Chores_iOS/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var startChoresButton: UIButton!
     
     func setButtonColor() {
-        startChoresButton.setTitleColor( UIColor(red:0.20, green:0.40, blue:0.54, alpha:1.0), for: .normal)
+        //startChoresButton.setTitleColor( UIColor(red:0.20, green:0.40, blue:0.54, alpha:1.0), for: .normal)
+        startChoresButton.setTitleColor(UIColor.white, for: .normal)
     }
     
     override func viewDidLoad() {
@@ -21,6 +22,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         setUpBackgroundColor()
         setButtonColor()
+        setUpGradient()
+    }
+    
+    func setUpGradient() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        let gradient = CAGradientLayer()
+        let top = UIColor(red:0.93, green:0.80, blue:0.64, alpha:1.0)
+        let bottom = UIColor(red:0.94, green:0.38, blue:0.62, alpha:1.0)
+        gradient.frame = view.bounds
+        gradient.colors = [top.cgColor, bottom.cgColor]
+        self.view.layer.insertSublayer(gradient, at: 0)
     }
     
     func setUpBackgroundColor() {
